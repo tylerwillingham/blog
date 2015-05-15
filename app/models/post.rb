b3c10a302f3f8592c00870d80424cf2754a1191c
Tyler Willingham <tylerwillingham@gmail.com>
2015-05-14 20:47:27 -0500

    Create routes & controller for Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :slug, presence: true, uniqueness: true
   validates :body, presence: true
+
+  default_scope { where(published: true) }
 end
