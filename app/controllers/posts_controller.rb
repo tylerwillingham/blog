3780dfe94397517ad7b5b4af37eab1ff4801aa42
Tyler Willingham <tylerwillingham@gmail.com>
2015-05-14 22:09:19 -0500

    Start integrating "Clean Blog" theme

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.limit(5)
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
 
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :show]
 end
